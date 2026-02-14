3412a2b5f7a18b3c3ab3c53db06b3cf9e779b4b1
JeanLDev <jean__lucas2013@hotmail.com>
2026-02-13 23:11:30 -0300

    AAAAA

---
+++ index.js
@@ -0,0 +1,27 @@
+// Incluir as bibliotecas
+// Gerencia as requisições, rotas e URLs, entre outra funcionalidades
+const express = require("express");
+// Chamar a função express
+const app = express();
+
+// Criar as rotas
+app.get("/", function(req, res){
+    res.send("Gerenciador Financeiro NOVO");
+});
+
+app.get("/sobre-empresa", function(req, res){
+    res.send("Pagina sobre empresa");
+});
+
+app.get("/blog", function(req, res){
+    res.send("Pagina do blog");
+});
+
+app.get("/contato", function(req, res){
+    res.send("Pagina de contato");
+});
+
+// Iniciar o servidor na porta 8080
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
